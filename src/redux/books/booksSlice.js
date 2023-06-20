5a1f22a90f7ed72e79947bdd15d5fadc6bb91dc8
rachaouldbabaali <rachaould@gmail.com>
2023-06-20 16:10:34 +0100

    Fix id issue

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -4,19 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 const initialState = {
   books: [
     {
-      item_id: uuidv4(),
+      id: uuidv4(),
       title: 'The Great Gatsby',
       author: 'John Smith',
       category: 'Fiction',
     },
     {
-      item_id: uuidv4(),
+      id: uuidv4(),
       title: 'Anna Karenina',
       author: 'Leo Tolstoy',
       category: 'Fiction',
     },
     {
-      item_id: uuidv4(),
+      id: uuidv4(),
       title: 'The Selfish Gene',
       author: 'Richard Dawkins',
       category: 'Nonfiction',
